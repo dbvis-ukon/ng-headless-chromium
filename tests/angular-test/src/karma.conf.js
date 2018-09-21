3185ff364545c7467741cf17fe4627e2cf7ad83c
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2018-09-21 09:26:44 -0700

    adjust karma.conf.js

--- tests/angular-test/src/karma.conf.js
+++ tests/angular-test/src/karma.conf.js
@@ -1,5 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+// karma.conf.js
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -25,7 +27,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox']
+      }
+    },
     singleRun: false
   });
 };
